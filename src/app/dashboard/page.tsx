8ef1cd914c2815a341e7065631e1832dfa2d2014
Saikh Seraj Fahim <saikhseraj2017@gmail.com>
2025-09-09 02:41:12 +0600

    top-part

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { APIProvider } from "@vis.gl/react-google-maps";
 import { Map, Marker } from "@vis.gl/react-google-maps";
+import Image from "next/image";
 import { useState } from "react";
 
 export default function Dashboard() {
@@ -9,7 +10,7 @@ export default function Dashboard() {
         lng: -79.9959,
     });
     return (
-        <div className="w-full h-[900px]">
+        <div className="w-full h-screen relative">
             <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ""}>
                 <Map
                     style={{ borderRadius: "20px" }}
@@ -21,6 +22,29 @@ export default function Dashboard() {
                     <Marker position={markerLocation} />
                 </Map>
             </APIProvider>
+
+            <div className="absolute top-4 w-full flex justify-between">
+                <div className="flex max-w-[320px] h-[30px] ml-8 mt-2 ring-2 ring-[#000000] 
+            bg-[#000000] rounded-md">
+                    <Image src="/search.svg" alt="search-icon" width={20} height={20}
+                        className="invert ml-2" />
+                    <input type="text" className="outline-none pl-2 font-poppins" />
+                    <button className="bg-[#2C2B2B] font-poppins text-[#FFFFFF] px-2 mr-6 rounded-md">
+                        Search</button>
+                </div>
+
+                <div>
+                    <Image src="/HomePageLogo.svg" alt="1st_page" width={300} height={100} />
+                </div>
+
+                <div className="flex flex-col gap-2">
+                    <p className="text-black font-poppins">Maya</p>
+                    <div className="w-12 h-12 relative mr-8">
+                        <Image src="/User.jpg" alt="user" fill className="rounded-full object-cover 
+                        ring-3 ring-[#00308F]"/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
