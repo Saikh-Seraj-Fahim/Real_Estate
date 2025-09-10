d7d86bebbfb556b2b9916dd9023abbfd8e69e9ee
Saikh Seraj Fahim <saikhseraj2017@gmail.com>
2025-09-11 00:17:03 +0600

    account

---
+++ src/app/account_settings/page.tsx
@@ -0,0 +1,10 @@
+export default function AccountSettings(){
+    return(
+        <div className="w-full h-screen">
+            <div className="w-1/4 h-screen flexborder border-amber-500">
+                <h1 className="text-[#000000] font-poppins font-semibold text-3xl">Account Settings</h1>
+            </div>
+            <p>hello</p>
+        </div>
+    );
+}
